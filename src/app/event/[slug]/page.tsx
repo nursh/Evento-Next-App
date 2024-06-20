61b7c62af06370fa6e095708f31b97c7405dea4c
nur-sh <nsheikhmohammed01@gmail.com>
2024-06-20 18:44:57 -0400

    Create header for event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,8 +1,61 @@
-export default function EventPage() {
+import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const { slug } = params;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event: EventoEvent = await response.json();
 
   return (
-    <>
-      <div>Some Random Event</div>
-    </>
-  )
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          sizes="(max-width: 1280) 100vw, 1280px"
+          className="object-cover blur-3xl z-0 "
+          quality={50}
+          priority
+        />
+
+        <div className="z-1 relative flex flex-col gap-6 lg:gap-16 lg:flex-row">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className="text-white/75 ">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="mb-2 whitespace-nowrap lg:text-5xl">{event.name}</H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 bg-blur mt-5 state-effects">Get tickets</button>
+          </div>
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -9,7 +9,7 @@ type EventCardProps = {
 export default function EventCard({ event }: EventCardProps) {
   return (
     <Link href={`/event/${event.slug}`} className=" flex-1 basis-80 h-[380px] max-w-[500px]">
-      <section className="flex flex-col w-full h-full bg-white/[3%] rounded-xl overflow-hidden relative hover:scale-105 active:scale[1.02] transition">
+      <section className="flex flex-col w-full h-full bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
           src={event.imageUrl}
           alt={event.name}
