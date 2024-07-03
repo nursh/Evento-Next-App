f7f03fc5e91908c9208d099c35c54d8f9cbc1cdc
nur-sh <nsheikhmohammed01@gmail.com>
2024-07-02 20:06:27 -0400

    add framer-motion card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,14 +1,38 @@
+"use client";
+
 import { EventoEvent } from "@/lib/types";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
+const MotionLink = motion(Link);
+
 export default function EventCard({ event }: EventCardProps) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
   return (
-    <Link href={`/event/${event.slug}`} className=" flex-1 basis-80 h-[380px] max-w-[500px]">
+    <MotionLink
+      ref={ref}
+      href={`/event/${event.slug}`}
+      className=" flex-1 basis-80 h-[380px] max-w-[500px]"
+      // @ts-ignore
+      style={{ scale: scaleProgress, opacity: opacityProgress }}
+      initial={{
+        opacity: 0,
+        scale: 0.8
+      }}
+    >
       <section className="flex flex-col w-full h-full bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
           src={event.imageUrl}
@@ -23,14 +47,18 @@ export default function EventCard({ event }: EventCardProps) {
           <p className="text-sm text-white/50 mt-4">{event.location}</p>
         </div>
         <section className="flex flex-col justify-center items-center absolute left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
-          <p className="text-xl font-bold -mb-[5px]">{new Date(event.date).toLocaleDateString('en-US', { day: "2-digit" })}</p>
+          <p className="text-xl font-bold -mb-[5px]">
+            {new Date(event.date).toLocaleDateString("en-US", {
+              day: "2-digit",
+            })}
+          </p>
           <p className="text-xs uppercase text-accent">
-            {
-              new Date(event.date).toLocaleDateString("en-US", { month: "short"})
-            }
+            {new Date(event.date).toLocaleDateString("en-US", {
+              month: "short",
+            })}
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
