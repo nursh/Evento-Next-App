2b869a76028acdb4807e4f45c7d79726d7918e30
nur-sh <nsheikhmohammed01@gmail.com>
2024-07-03 04:28:13 -0400

    Create Prisma database

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { twMerge } from 'tailwind-merge';
 import clsx, { ClassValue } from "clsx";
-import { EventoEvent } from './types';
+import prisma from './db';
+import { notFound } from 'next/navigation';
+
 
 export function cn(...classes: ClassValue[]) {
  return twMerge(clsx(classes));
@@ -12,16 +14,29 @@ export function capitalize(str: string) {
 
 
 export async function getEvents(city: string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
-    { next: { revalidate: 300 } }
-  );
-  return response.json();
+  const searchCity = city === 'all' ? undefined : capitalize(city);
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: searchCity
+    },
+    orderBy: {
+      date: 'asc'
+    }
+  });
+  return events;
 }
 
 export async function getEvent(slug: string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  return response.json();
+
+  const event = await prisma.eventoEvent.findUnique({
+    where: {
+      slug
+    }
+  });
+
+  if (!event) {
+    return notFound();
+  }
+
+  return event;
 }
